3eb85aafce7ea87b9c0ea5f7cc6cd7348bf26695
t4tools <professort4@gmail.com>
2019-12-03 22:58:43 +0700

    test

--- krtools.js
+++ krtools.js
@@ -269,8 +269,6 @@ function cripple_window(_window) {
 			// silent aim
 			inputs[xDr] = +(tx % PI2).toFixed(3);
 			inputs[yDr] = +(ty % PI2).toFixed(3);
-			inputs[9] = +(tx % PI2).toFixed(3);
-			inputs[10] = +(ty % PI2).toFixed(3);
 
 			// auto reload
 			controls.keys[controls.reloadKey] = !haveAmmo() * 1;
